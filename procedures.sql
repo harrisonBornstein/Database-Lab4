4ee8ba0d40479c3fe904b927a6584f5e486671de
dit3637 <harrison.bornstein@absg.com>
2014-12-16 12:00:43 +0100

    Added Procedures

---
+++ procedures.sql
@@ -0,0 +1,24 @@
+	
+DELIMITER //
+CREATE PROCEDURE createReservation(IN flight INT(8))
+BEGIN
+	INSERT INTO RESERVATION (flight) VALUES (flight);
+END//
+DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE addPDetails(IN reservation INT(8),IN ssn INT(10), IN fname VARCHAR(25), IN lname VARCHAR(25), IN email VARCHAR(60), phoneNumber VARCHAR(15))
+BEGIN
+	INSERT INTO PASSENGER (id,FName,LName) VALUES (ssn,fname,lname);
+	INSERT INTO PGROUP (passenger, reservation) VALUES (ssn, reservation);
+	IF email IS NOT NULL THEN
+		INSERT INTO CUSTOMER (passengerID,email,phoneNumber) VALUES (ssn,emailphoneNumber);
+		UPDATE RESERVATION 
+		SET customer = ssn
+		WHERE id = reservation;
+	END IF	
+END//
+DELIMITER ;
+
+
